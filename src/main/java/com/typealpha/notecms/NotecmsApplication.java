34d9e71082d81a9ec3b5faf0c2499a25509b913b
AlexCZhou <zbc1357@hotmail.com>
2020-12-31 11:44:00 +0800

    init

---
+++ src/main/java/com/typealpha/notecms/NotecmsApplication.java
@@ -0,0 +1,13 @@
+package com.typealpha.notecms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotecmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotecmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/typealpha/notecms/NotecmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typealpha.notecms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotecmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
